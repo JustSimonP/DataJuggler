b1359b83d9b5d60049aa6bb3377987b7eb4b5a0b
SimonP <szymonpajdo@gmail.com>
2023-03-21 19:18:09 +0100

    initial file format reading

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,14 @@ use std::path::PathBuf;
 
 fn main() {
     println!("Hello, world!");
-   // let dir= env::current_dir().unwrap();
-    let current_dir = fs::read_dir("C:/Users/User").unwrap();
+    let dir= env::current_dir().unwrap();
+    let current_dir = fs::read_dir(dir).unwrap();
     current_dir.for_each(|item| {
         let mk = item.unwrap().path();
        if mk.is_file() && mk.extension().is_some() {
            let mir : String =  mk.extension().and_then(OsStr::to_str).unwrap().to_string();
-           println!("ifeiifi {}", &mir)
+           println!("File format in path {}", &mir)
        }
     })
-    // let huj = fs::read_dir(current_dir).unwrap();
 }
-// fn listJsonsByDateModified() {
-//     let current_dir = env::current_dir().unwrap().as_ref();
-//
-//        // entry.unwrap().metadata().unwrap().file_type()
-//     let huj = fs::read_dir(current_dir).unwrap();
-//     // let jsons = fs::read_dir(current_dir)
-//     //     .unwrap().filter(|x| x.unwrap().metadata().unwrap().file_type() == "json" );
-// }
 
-
-// println!("hello there!");
-// println!("format {} arguments", "some");
-// let local_variable = "some";
-// println!("format {local_variable} arguments");
