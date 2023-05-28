b8ddbfa573660e51b5aca8e9cfaefc5617ccda1e
SimonP <szymonpajdo@gmail.com>
2023-05-28 18:52:08 +0200

    Working json file path retrieval

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use std::{env, fs};
 use std::ffi::OsStr;
-use std::path::PathBuf;
-
+use std::path::{Path, PathBuf};
+use std::fs::File;
 fn main() {
     println!("Hello, world!");
     let dir= env::current_dir().unwrap();
     let current_dir = fs::read_dir(dir).unwrap();
+    let mut available_jsons = Vec::new();
     current_dir.for_each(|item| {
-        let mk = item.unwrap().path();
-       if mk.is_file() && mk.extension().is_some() {
-           let mir : String =  mk.extension().and_then(OsStr::to_str).unwrap().to_string();
-           println!("File format in path {}", &mir)
+        let retrieved_file = item.unwrap();
+        let file_path = &retrieved_file.path();
+       if file_path.is_file() && file_path.extension().is_some() {
+           let mir : String =  file_path.extension().and_then(OsStr::to_str).unwrap().to_string();
+           println!("File format in path {}", &mir);
+           if mir == "json" {
+               available_jsons.push((retrieved_file.file_name().into_string().unwrap(), file_path.clone()));
+           }
        }
-    })
+    });
+    println!("{}", available_jsons.is_empty());
+   // File::open()
 }
 
